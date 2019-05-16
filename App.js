2b4876135a0c70a6a1f87db86a5e54556f6fe7ba
johnopthof <jhof@avans.nl>
2019-05-16 11:25:42 +0200

    Added constructor (Git test)

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+
+  constructor(props){
+    super(props);
+    this.state ={ 
+      data: null,
+      loaded: true,
+      error:null,
+      text: '',
+      }
+  }
+
+  baseURL = 'http://b03zm72.locgov.nl:8080';
+
+
 
 type Props = {};
 export default class App extends Component<Props> {
