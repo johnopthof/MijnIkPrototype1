4a1d3b791323b298cfe18cad193d054d06a0c93e
johnopthof <jhof@avans.nl>
2019-05-21 11:00:13 +0200

    Installed react-navigation

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-
+import { createStackNavigator, createAppContainer } from "react-navigation";
 import LoginComponent from './components/LoginComponent.js';
 
 type Props = {};
